ab88848a3ac9efb95a5c0bc4d13713b4175a6b9d
Yannik Korzikowski <yannik@korzikowski.de>
2020-04-30 16:22:56 +0200

    delete dump from container if aws cp fails

--- dumpAndCopy.sh
+++ dumpAndCopy.sh
@@ -3,7 +3,7 @@ set -eux
 
 ./dumpDatabase.sh
 if [ $? -ne 1 ]; then
-    aws s3 cp /pg_backup/*.dump "$S3"
+    aws s3 cp /pg_backup/*.dump "$S3" || rm /pg_backup/*
     rm /pg_backup/*
     exit 0
 fi
