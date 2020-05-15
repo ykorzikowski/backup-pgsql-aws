856be7adfd13682f94f429ebc0fc5a7414ef15aa
Yannik Korzikowski <git@korzikowski.de>
2020-05-15 14:44:16 +0200

    Update dumpAndCopy.sh

--- dumpAndCopy.sh
+++ dumpAndCopy.sh
@@ -3,7 +3,7 @@ set -eux
 
 ./dumpDatabase.sh
 if [ $? -ne 1 ]; then
-    aws s3 cp /pg_backup/*.dump "$S3" || rm /pg_backup/*
+    aws s3 --endpoint-url $ENDPOINT_URL cp /pg_backup/*.dump "$S3" || rm /pg_backup/*
     rm /pg_backup/*
     exit 0
 fi
